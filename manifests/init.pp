878b41d4742aaa3ec28ef1b0dcb8e385cca7e4e1
Joshua Hoblitt <josh@hoblitt.com>
2022-08-18 20:47:42 -0700

    change enable_plugins param processing to use iteration
    Puppet iteration didn't exist at the time of the original implementation.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,9 +2,9 @@
 #
 class awstats (
   Boolean $config_dir_purge = false,
-  Array $enable_plugins   = [],
-  String $owner = $awstats::params::owner,
-  String $group = $awstats::params::group,
+  Array $enable_plugins     = [],
+  String $owner             = $awstats::params::owner,
+  String $group             = $awstats::params::group,
 ) inherits awstats::params {
   package { $awstats::params::package_name: }
   -> file { $awstats::params::config_dir_path:
@@ -16,8 +16,7 @@ class awstats (
     purge   => $config_dir_purge,
   }
 
-  if size($enable_plugins) > 0 {
-    $load = prefix(downcase($enable_plugins), 'awstats::plugin::')
-    contain $load
+  $enable_plugins.each |$plugin| {
+    contain "awstats::plugin::${plugin}"
   }
 }
