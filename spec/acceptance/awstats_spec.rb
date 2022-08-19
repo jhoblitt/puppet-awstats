798655c7a6ca7aabb95e11948f46c895493ef990
Joshua Hoblitt <josh@hoblitt.com>
2022-08-18 22:34:18 -0700

    modernize acceptance spec

--- spec/acceptance/awstats_spec.rb
+++ spec/acceptance/awstats_spec.rb
@@ -3,64 +3,9 @@
 require 'spec_helper_acceptance'
 
 describe 'awstats class' do
-  fqdn = fact 'fqdn'
-  hostname = fact 'hostname'
+  hostname = fact('hostname')
 
-  describe 'running puppet code' do
-    pp = <<-EOS
-      if $::osfamily == 'RedHat' {
-        class { '::epel': } -> Class['::awstats']
-      }
-
-      include ::apache
-
-      apache::vhost { 'awstats.example.org':
-        port          => '80',
-        docroot       => '/usr/share/awstats/wwwroot',
-        serveraliases => ['awstats'],
-        aliases => [
-          { alias => '/awstatsclasses', path => '/usr/share/awstats/wwwroot/classes/' },
-          { alias => '/awstatscss', path => '/usr/share/awstats/wwwroot/css/' },
-          { alias => '/awstatsicons', path => '/usr/share/awstats/wwwroot/icon/' },
-        ],
-        scriptaliases => [
-          { alias => '/awstats/', path => '/usr/share/awstats/wwwroot/cgi-bin/' },
-        ],
-        directories   => [{
-          path     => '/usr/share/awstats/wwwroot',
-          provider => 'directory',
-          options  => 'None',
-        }],
-        setenv        => ['PERL5LIB /usr/share/awstats/lib:/usr/share/awstats/plugins'],
-      }
-
-      class { '::awstats':
-        config_dir_purge => true,
-        enable_plugins   => [ 'DecodeUTFKeys', 'GeoIP' ],
-      }
-
-      # this ordering is needed for both the docroot path and so that the
-      # awstats package provided apache configuration snippet is purged on the
-      # first run
-      Class['::awstats'] -> Class['::apache']
-
-      awstats::conf { 'defaults.example.org': }
-
-      awstats::conf { 'tweaked.example.org':
-        options => {
-          'AllowFullYearView' => 2,
-          'DNSLookup'         => 1,
-          'LoadPlugin'        => ['decodeutfkeys', 'geoip'],
-          'SiteDomain'        => 'tweaked.example.org',
-        },
-      }
-    EOS
-
-    it 'applies the manifest twice with no stderr' do
-      apply_manifest(pp, catch_failures: true)
-      apply_manifest(pp, catch_changes: true)
-    end
-  end
+  include_examples 'the example', 'awstats.pp'
 
   %w[
     awstats
@@ -91,7 +36,7 @@ describe 'awstats class' do
         HostAliases="localhost 127.0.0.1 #{hostname}"
         LogFile="/var/log/httpd/access_log"
         LogFormat=1
-        SiteDomain="#{fqdn}"
+        SiteDomain="defaults.example.org"
       EOS
     end
   end
